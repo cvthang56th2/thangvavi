d0464d193d7ff37797bd90df1ca2a2ce7586d578
cvthang56th2 <cvthang56th2@gmail.com>
2019-06-29 00:23:09 +0700

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+// Routes
+// app.get('/', function(req, res) {
+//   res.render('index');
+// })
+app.get('/', function(req, res) {
+  res.render('love1');
+})
+
+app.get('/love1', function(req, res) {
+  res.render('love1');
+})
+
+app.get('/love2', function(req, res) {
+  res.render('love2');
+})
